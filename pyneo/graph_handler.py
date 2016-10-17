b9ce310c1975d8078c00cd18ace58b6da5101bfa
Arnaud Paran <paran.arnaud@gmail.com>
2016-10-17 15:27:20 +0200

    some documentation added, protect function removed, and parameters handling added

--- pyneo/graph_handler.py
+++ pyneo/graph_handler.py
@@ -54,57 +54,147 @@ class GraphHandler(object):
         """
         base64string = base64.encodestring('{}:{}'.format(username,
                                                           password))[:-1]
-        self.authheader = "Basic {}".format(base64string)
-        self.query_url = "http://{0}/db/data/cypher".format(host_port)
-        self.graph_query_url = "http://{0}/db/data/transaction/commit"\
+        self.authheader = 'Basic {}'.format(base64string)
+        self.query_url = 'http://{0}/db/data/cypher'.format(host_port)
+        self.graph_query_url = 'http://{0}/db/data/transaction/commit'\
             .format(host_port)
 
-    def send_query(self, query):
-        """
-        The important function, it sends a cypher query to the neo4j database
-        and returns the formatted result
+    def send_query(self, query, **params):
+        """the function that sends normal cypher queries to the server
+
+            - parameters using ``:param <name>: <description>``
+            - type of the parameters ``:type <name>: <description>``
+            - returns using ``:returns: <description>``
+            - examples (doctest)
+            - seealso using ``.. seealso:: text``
+            - notes using ``.. note:: text``
+            - warning using ``.. warning:: text``
+            - todo ``.. todo:: text``
+
+            :param query: a cyhper query to execute on the server
+            :param params: the parameters passed to the cypher request
+            :type query: string
+            :type params: dict
+
+            .. warning:: we would strongly advise you to use parameters
+            if you want to avoid having to face cypher injection security
+            issues
+
+            :Example:
+
+                >>> graph_handler.\
+                >>>     send_query('MATCH (n) RETURN ID(n) AS id LIMIT 25')
+                [{'id': 0}, {'id': 1}, ..., {'id': 23}, {'id': 24}]
+
+                And with parameters
+
+                >>> graph_handler.\
+                >>>     send_query('MATCH (n {name: {p}}) RETURN ID(n)',
+                >>>                p="test")
+                [{'id': 20}]
         """
         query_params = {}
-        query_params["query"] = query
+        query_params['query'] = query
+        query_params['params'] = params
+
         req = urllib2.Request(self.query_url,
                               data=json.dumps(query_params))
         req.add_header('Content-Type', 'application/json')
-        req.add_header("Authorization", self.authheader)
+        req.add_header('Accept', 'application/json')
+        req.add_header('Authorization', self.authheader)
+
         handle = urllib2.urlopen(req)
+
         response_dict = json.loads(handle.read())
         formatted_response = GraphHandler.format_query_response(response_dict)
+
         return formatted_response
 
-    def ask_graph(self, query):
-        """
-        The important function, it sends a graph cypher query to the neo4j
-        database and returns the formatted result
+    def ask_graph(self, query, **params):
+        """the function that sends cypher queries that should retrive neo4j graphs
+        in order to display it in a d3js force element
+
+            - parameters using ``:param <name>: <description>``
+            - type of the parameters ``:type <name>: <description>``
+            - returns using ``:returns: <description>``
+            - examples (doctest)
+            - seealso using ``.. seealso:: text``
+            - notes using ``.. note:: text``
+            - warning using ``.. warning:: text``
+            - todo ``.. todo:: text``
+
+            :param query: a cyhper query to execute on the server to
+            select the graph to retrive
+            :param params: a dict of parameters to pass with the request
+            :type query: string
+            :type params: dict
+
+            :Example:
+
+                >>> graph_handler.\
+                >>>     ask_graph('MATCH path = (n)-[r]->(m) RETURN path')
+                {
+                'results': [
+                    {
+                        'columns': ['path'],
+                        'data': [
+                            {
+                                'graph': {
+                                    'nodes': [{...}, {...}],
+                                    'relationships': [{...}]
+                            },
+                            {
+                                'graph': ...
+                            },
+                            ...
+                        ]
+                    }
+                ],
+                'errors': []
+                }
         """
         query_params = {}
-        query_params["statement"] = query
-        query_params["resultDataContents"] = ["graph"]
-        query_params = {"statements": [query_params]}
+        query_params['statement'] = query
+        query_params['resultDataContents'] = ['graph']
+        query_params['parameters'] = params
+        query_params = {'statements': [query_params]}
+
         req = urllib2.Request(self.graph_query_url,
                               data=json.dumps(query_params))
         req.add_header('Content-Type', 'application/json')
-        req.add_header("Authorization", self.authheader)
+        req.add_header('Accept', 'application/json')
+        req.add_header('Authorization', self.authheader)
+
         handle = urllib2.urlopen(req)
+
         response_dict = json.loads(handle.read())
+
         return response_dict
 
     @staticmethod
     def format_query_response(response_dict):
-        """Formats the response recieved by the server"""
-        col_names = response_dict["columns"]
+        """Formats the response recieved by the server
+        it essentially seperates the data in order to return
+        a single array of dicts, each dict representing one row
+        of the data
+
+            - parameters using ``:param <name>: <description>``
+            - type of the parameters ``:type <name>: <description>``
+            - returns using ``:returns: <description>``
+            - examples (doctest)
+            - seealso using ``.. seealso:: text``
+            - notes using ``.. note:: text``
+            - warning using ``.. warning:: text``
+            - todo ``.. todo:: text``
+
+            :param response_dict: the dict returned by the server
+            :type response_dict: dict
+        """
+        col_names = response_dict['columns']
         formatted_response = []
-        for line in response_dict["data"]:
+        for line in response_dict['data']:
             formatted_line = {}
             for col_name, cell in zip(col_names, line):
                 formatted_line[col_name] = cell
             formatted_response.append(formatted_line)
         return formatted_response
-
-    @staticmethod
-    def protect(string):
-        """replaces the \' in strings by \\\', one kind of security"""
-        return string.replace("'", "\\\'").replace("\"", "\\\"")
--- pyneo/tests/test_graph_handler.py
+++ pyneo/tests/test_graph_handler.py
@@ -9,43 +9,26 @@ from pyneo import GraphHandler
 class TestGraphHandler(unittest.TestCase):
     """tests the GraphHandler class"""
 
-    def assert_eq(self, first, second, message=""):
+    def assert_eq(self, first, second, message=''):
         """Redefiniton of assertEqual with enchanced error message.
         in order to be more readable, it shows the two values"""
         self.assertEqual(
             first,
             second,
-            "expects {} to be {}.\n".format(first, second) + message)
+            'expects {} to be {}.\n'.format(first, second) + message)
 
     def setUp(self):
-        self.graph_handler = GraphHandler("localhost:7474", "neo4j", "neo4j")
+        self.graph_handler = GraphHandler('localhost:7474', 'neo4j', 'neo4j')
 
     def test_format_query_response(self):
         """Tests that the responses are correctly formatted"""
         response = {
-            "columns": ["a", "b"],
-            "data": [
+            'columns': ['a', 'b'],
+            'data': [
                 [None, 1],
                 [None, 2]
             ]
         }
         formatted_response =\
             self.graph_handler.format_query_response(response)
-        self.assert_eq(formatted_response[0]["b"], 1)
-
-    def test_protect(self):
-        """ensures that the protect function does protect correctly
-        strings, for example
-            >>> g.protect('"hey"')
-            '\"hey\"'
-        """
-        test_str = [
-            {'orig': 'Hey', 'protected': 'Hey'},
-            {'orig': '"Hey"', 'protected': '\\"Hey\\"'},
-            {'orig': '\"Hey\"', 'protected': '\\\"Hey\\\"'},
-            {'orig': '\'Hey\'', 'protected': '\\\'Hey\\\''}
-            ]
-
-        for tested in test_str:
-            protected = self.graph_handler.protect(tested['orig'])
-            self.assert_eq(protected, tested['protected'])
+        self.assert_eq(formatted_response[0]['b'], 1)
